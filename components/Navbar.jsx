4696ae3b48f72593c42fea24f393d6a307e56bcd
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-10 00:52:22 +0600

    add the login and register link in navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -71,24 +71,28 @@ const Navbar = () => {
             </div>
 
             <div className="w-full min-w-max space-y-2 border-yellow-200 lg:space-y-0 sm:w-max lg:border-l ">
-              <button
-                type="button"
-                title="Start buying"
-                className="w-full py-3 px-6 text-center rounded-full transition active:bg-yellow-200   focus:bg-yellow-100 sm:w-max"
-              >
-                <span className="block text-yellow-800 font-semibold text-sm">
-                  Sign up
-                </span>
-              </button>
-              <button
-                type="button"
-                title="Start buying"
-                className="w-full py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300 sm:w-max"
-              >
-                <span className="block text-yellow-900 font-semibold text-sm">
-                  Login
-                </span>
-              </button>
+              <Link href="/register">
+                <button
+                  type="button"
+                  title="Start buying"
+                  className="w-full py-3 px-6 text-center  transition  sm:w-max"
+                >
+                  <span className="block text-yellow-800 font-semibold text-sm">
+                    Sign up
+                  </span>
+                </button>
+              </Link>
+              <Link href="/login">
+                <button
+                  type="button"
+                  title="Start buying"
+                  className="w-full py-3 px-6 text-center rounded-full transition bg-yellow-300 hover:bg-yellow-100 active:bg-yellow-400 focus:bg-yellow-300 sm:w-max"
+                >
+                  <span className="block text-yellow-900 font-semibold text-sm">
+                    Login
+                  </span>
+                </button>
+              </Link>
             </div>
           </div>
         </div>
