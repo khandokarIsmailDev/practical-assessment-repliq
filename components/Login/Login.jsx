5c975848cc6ef8fbc3d00547779317f71b1cf699
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-10 03:35:19 +0600

    add validation and connect with database

--- components/Login/Login.jsx
+++ components/Login/Login.jsx
@@ -1,6 +1,58 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import Link from "next/link";
+import React,{useState} from "react";
+import { toast } from "react-toastify";
 
 export default function Login() {
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const handleChange = e =>{
+        let name = e.target.name;
+        let value = e.target.value;
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = async e =>{
+        e.preventDefault();
+
+        if(!formData.email || !formData.password){
+            toast.error("Please fill all the fields");
+            return;
+        }
+
+        const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailPattern.test(formData.email)){
+            toast.error("Please enter a valid email address");
+            return;
+        }
+
+        const loadingToastId = toast.loading("Loading...");
+
+        try{
+            const response = await axios.post("/api/user/login", JSON.stringify(formData), {
+                headers:{
+                    "Content-Type": "application/json"
+                }
+            });
+
+            if(response.status === 200){
+                toast.update(loadingToastId, { render: "Login successful", type: "success", isLoading: false, autoClose: 3000 });
+            }
+        }catch(err){
+            toast.update(loadingToastId, { render: "Invalid email or password", type: "error", isLoading: false, autoClose: 3000 });
+        }
+
+    }
+
+    console.log(formData);
+
   return (
     <div className="pt-28">
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
@@ -26,8 +78,10 @@ export default function Login() {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  onChange={(e) => handleChange(e)}
+                  value={formData.email}
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                 />
               </div>
             </div>
@@ -54,9 +108,11 @@ export default function Login() {
                   id="password"
                   name="password"
                   type="password"
+                  onChange={(e) => handleChange(e)}
+                  value={formData.password}
                   autoComplete="current-password"
                   required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                  className="block w-full pl-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                 />
               </div>
             </div>
@@ -65,6 +121,7 @@ export default function Login() {
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-[#895317] px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-[#895417a8] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#895417a8]"
+                onClick={e=>handleSubmit(e)}
               >
                 Sign in
               </button>
@@ -73,12 +130,13 @@ export default function Login() {
 
           <p className="mt-10 text-center text-sm text-gray-500">
             Not a member?
-            <a
-              href="#"
+            <Link
+              href="/register"
               className="font-semibold text-[#895317] hover:text-[#895417a8]"
+              
             >
               Register
-            </a>
+            </Link>
           </p>
         </div>
       </div>
