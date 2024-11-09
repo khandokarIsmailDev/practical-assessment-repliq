01dd45a93b0c2cbbe8dbfe9185189ace77e79700
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-09 19:12:49 +0600

    registration api create and test

--- app/api/user/registration/route.js
+++ app/api/user/registration/route.js
@@ -1,5 +1,16 @@
+import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 
-export async function GET(req){
-    return NextResponse.json({message: "Hello World"})
+export async function POST(req,res){
+    try{
+        const reqBody = await req.json()
+        reqBody.otp = "0"
+        const prisma = new PrismaClient()
+        const result = await prisma.users.create({
+            data:reqBody
+        })
+        return NextResponse.json({status:"success",data:result})
+    }catch(err){
+        return NextResponse.json({status:"failed",data:err.toString()})
+    }
 }
