a969aceacd6ee1ddb88b801a39af4fddcf65b6b9
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-10 23:08:34 +0600

    fix or add id in queryKey for single data and fix loading animation

--- components/Recipes/SingleRecipe.jsx
+++ components/Recipes/SingleRecipe.jsx
@@ -5,11 +5,14 @@ import React from "react";
 
 const SingleRecipe = ({ id, setIsOpen }) => {
   const { data, isLoading, error } = useQuery({
-    queryKey: ["recipe-details"],
+    queryKey: ["recipe-details",id],
     queryFn: () => HttpKit.getRecipeDetails(id),
   });
 
-  if (!isLoading) return "Loading...";
+  console.log("here is singleRecipe id", id);
+  console.log("here is singleRecipe data", data);
+
+  if (isLoading) return "Loading...";
   return (
     <div className="flex flex-col gap-5">
       <div className="flex justify-end">
