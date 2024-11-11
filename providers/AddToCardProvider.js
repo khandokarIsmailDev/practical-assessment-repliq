056148cbc7b67af6eef587ebcd5cfb3a447fb901
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-11 17:24:27 +0600

    fix error

--- providers/AddToCardProvider.js
+++ providers/AddToCardProvider.js
@@ -5,14 +5,20 @@ import { AddToCardContext } from "@/context";
 export default function AddToCardProvider ({children}) {
     
     const [cart, setCart] = useState(() => {
-        const storedCart = localStorage.getItem("cart");
-        return storedCart ? JSON.parse(storedCart) : [];
-      });
+        // Check if window is defined to ensure we're in the browser
+        if (typeof window !== "undefined") {
+            const storedCart = localStorage.getItem("cart");
+            return storedCart ? JSON.parse(storedCart) : [];
+        }
+        return []; // Default value if not in the browser
+    });
     
-      // Update localStorage whenever cart changes
-      useEffect(() => {
-        localStorage.setItem("cart", JSON.stringify(cart));
-      }, [cart]);
+    // Update localStorage whenever cart changes
+    useEffect(() => {
+        if (typeof window !== "undefined") {
+            localStorage.setItem("cart", JSON.stringify(cart));
+        }
+    }, [cart]);
 
     return (
         <AddToCardContext.Provider value={{cart, setCart}}>
