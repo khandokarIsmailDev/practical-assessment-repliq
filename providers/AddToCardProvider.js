67ab3ba124e56187764ae71c65e9192c758a0d98
khandokarIsmailDev <ismailkhandokar000@gmail.com>
2024-11-11 14:06:12 +0600

    set localstroage in provider

--- providers/AddToCardProvider.js
+++ providers/AddToCardProvider.js
@@ -1,9 +1,18 @@
 "use client"
-import { useState, useContext } from "react";
+import { useState, useEffect } from "react";
 import { AddToCardContext } from "@/context";
 
 export default function AddToCardProvider ({children}) {
-    const [cart, setCart] = useState([])
+    
+    const [cart, setCart] = useState(() => {
+        const storedCart = localStorage.getItem("cart");
+        return storedCart ? JSON.parse(storedCart) : [];
+      });
+    
+      // Update localStorage whenever cart changes
+      useEffect(() => {
+        localStorage.setItem("cart", JSON.stringify(cart));
+      }, [cart]);
 
     return (
         <AddToCardContext.Provider value={{cart, setCart}}>
